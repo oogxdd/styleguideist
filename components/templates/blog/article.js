a0fb41f39ce8aa56fe24c953c6e28a5bcf29cad2
ignatif <ignatif@gmail.com>
2022-01-26 12:35:01 +0200

    wi[

--- components/templates/blog/article.js
+++ components/templates/blog/article.js
@@ -1,10 +1,6 @@
-import { MDXProvider } from '@mdx-js/react'
-import Article from './article/sample.mdx'
+// import Article from './article/sample.mdx'
 
 export const ArticlePage = () => (
   <div className="flex flex-col max-w-2xl container prose prose-sm md:prose self-start pb-28">
-    <MDXProvider>
-      <Article />
-    </MDXProvider>
   </div>
 )
